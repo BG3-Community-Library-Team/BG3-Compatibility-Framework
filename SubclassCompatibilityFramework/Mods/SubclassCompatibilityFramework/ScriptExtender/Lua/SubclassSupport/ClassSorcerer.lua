4193b72b73700b2bf8a1947b383e9568b4915c05
CodyAbode <cody@rocketship.com>
2023-09-15 09:20:18 -0700

    Fixed modGuids

--- SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/SubclassSupport/ClassSorcerer.lua
+++ SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/SubclassSupport/ClassSorcerer.lua
@@ -3,13 +3,13 @@ Ext.Require("SubclassSupport/_SCF_Globals.lua")
 -- Subclass Definitions
 local subClasses = {
   BaaleosShadowSorcerer = { -- BG3ShadowSorcerer by Baaleos
-    modGuid = "f48b2612-4d95-4601-b324-b8a07c9d90be",
+    modGuid = "38291fe1-40a3-4af7-bbbd-c9a55f3e39c0",
     subClassGuid = "d007a7be-60c6-4606-87db-66eb9e247050",
     class = "sorcerer",
     subClassName = "Shadow Sorcerery"
   },
   CodyAbodeStoneSorcery = { -- Stone Sorcery by Cody Abode
-  modGuid = "1be07b69-db2d-4df4-9f3f-1b00fd07e2cb",
+  modGuid = "f48b2612-4d95-4601-b324-b8a07c9d90be",
   subClassGuid = "3c335f43-bae3-4468-9972-d9bdedca2a71",
   class = "sorcerer",
   subClassName = "Stone Sorcery"
