e7951ccb71dbc199875ef730904d253a20f28d11
NellsRelo <nellsrelo@gmail.com>
2023-09-15 15:47:33 -0700

    Include API Use Template

---
+++ API Use Template/Mods/ModName/ScriptExtender/Lua/BootstrapServer.lua
@@ -0,0 +1,18 @@
+if Ext.Mod.IsModLoaded("67fbbd53-7c7d-4cfa-9409-6d737b4d92a9") then
+  local subClasses = {
+    AuthorSubclass = {
+      modGuid = "your-mods-uuid-in-metalsx",
+      subClassGuid = "your-subclass-class-description-uuid",
+      class = "lowercase parent class name",
+      subClassName = "English-localized name for your class (Optional)"
+    }
+  }
+
+  local function OnSessionLoaded()
+    Mods.SubclassCompatibilityFramework = Mods.SubclassCompatibilityFramework or {}
+    Mods.SubclassCompatibilityFramework.API = Mods.SubclassCompatibilityFramework.Api or {}
+    Mods.SubclassCompatibilityFramework.API.InsertSubClasses(subClasses)
+  end
+
+  Ext.Events.SessionLoaded:Subscribe(OnSessionLoaded)
+end
