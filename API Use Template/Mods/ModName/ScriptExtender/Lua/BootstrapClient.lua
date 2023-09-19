af6f24ab7e585b843f561c80b3e9b6222c33a325
NellsRelo <nellsrelo@gmail.com>
2023-09-19 10:00:25 -0700

    Update API Use Template

--- API Use Template/Mods/ModName/ScriptExtender/Lua/BootstrapClient.lua
+++ API Use Template/Mods/ModName/ScriptExtender/Lua/BootstrapClient.lua
@@ -3,7 +3,7 @@ if Ext.Mod.IsModLoaded("67fbbd53-7c7d-4cfa-9409-6d737b4d92a9") then
     AuthorSubclass = {
       modGuid = "your-mods-uuid-in-metalsx",
       subClassGuid = "your-subclass-class-description-uuid",
-      class = "lowercase parent class name",
+      class = "lowercase parent class name or UUID for class progression granting subclass choices",
       subClassName = "English-localized name for your class (Optional)"
     }
   }
