a44926cc715a458c8c52cd2859151f03b4f2921d
NellsRelo <nellsrelo@gmail.com>
2023-09-27 10:08:34 -0700

    Clean up Initializers

--- SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/BootstrapClient.lua
+++ SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/BootstrapClient.lua
@@ -1,1 +1,15 @@
-Ext.Require("InitializeFramework.lua")
+Ext.Require("Init/_InitGlobals.lua")
+Ext.Require("CompatibilitySupport/SubclassSupport/_IntegratedSubClasses.lua")
+Ext.Require("CompatibilitySupport/_Strings.lua")
+Ext.Require("CompatibilitySupport/_Globals.lua")
+Ext.Require("CompatibilitySupport/_Utils.lua")
+Ext.Require("CompatibilitySupport/SubclassSupport/_SubclassCompatibility.lua")
+Ext.Require("CompatibilitySupport/SelectorSupport/_SelectorCompatibility.lua")
+Ext.Require("CompatibilitySupport/StringSupport/_ProgressionStringCompatibility.lua")
+Ext.Require("CompatibilitySupport/ListsSupport/_ListCompatibility.lua")
+Ext.Require("CompatibilitySupport/API.lua")
+
+Utils.Info(Strings.LOADED_REQUIRED_FILES)
+
+Ext.Require("Init/_InitClasses.lua")
+Ext.Require("Init/_InitIntegratedSubclasses.lua")
---
+++ SubclassCompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Init/_InitGlobals.lua
@@ -0,0 +1,10 @@
+SCF = {}
+SCF.modTableKey = "SubclassCompatibilityFramework"
+SCF.modPrefix = "SCF"
+SCF.modVersion = { major = 2, minor = 3, revision = 0, build = 0 }
+
+Api = {}
+Globals = {}
+Utils = {}
+Strings = {}
+IntegratedSupport = {}
