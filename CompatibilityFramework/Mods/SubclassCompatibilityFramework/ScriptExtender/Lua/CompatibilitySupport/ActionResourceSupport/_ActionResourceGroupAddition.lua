f49bcbcf467909303d10886b944cf8df5e37bcf6
NellsRelo <nellsrelo@gmail.com>
2023-09-29 23:25:56 -0700

    Add Endpoint and Logic for ActionResourceGroup Addition + Removal

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/CompatibilitySupport/ActionResourceSupport/_ActionResourceGroupAddition.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/CompatibilitySupport/ActionResourceSupport/_ActionResourceGroupAddition.lua
@@ -1,19 +1,12 @@
 local function AddActionResourceGroup(payload)
-    local resourceGroupArr = Utils.CacheOrRetrieve(payload.TargetUUID, "ActionResourceGroup").ActionResourceDefinitions
-    resourceGroupArr = Utils.MergeTables(resourceGroupArr, payload.Definitions)
+  local resourceGroupArr = Utils.CacheOrRetrieve(payload.TargetUUID, "ActionResourceGroup").ActionResourceDefinitions
+  resourceGroupArr = Utils.MergeTables(resourceGroupArr, payload.Definitions)
 end
 
 function HandleActionResourceGroupAddition(payload)
-    if payload ~= nil then
-        AddActionResourceGroup(payload)
-    else
-        Utils.Error(Strings.ERROR_EMPTY_PAYLOAD)
-    end
+  if payload ~= nil then
+    AddActionResourceGroup(payload)
+  else
+    Utils.Error(Strings.ERROR_EMPTY_PAYLOAD)
+  end
 end
-
-local payload = {
-    TargetUUID = "",
-    Definitions = {
-      "", "", "", ...
-    }
-}
