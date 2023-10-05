2a8540c5856575d481343d9303fb304e74fb8166
NellsRelo <nellsrelo@gmail.com>
2023-10-04 20:16:14 -0700

    Expand Dictionary Structure

---
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/API/DictionaryApi.lua
@@ -0,0 +1,1 @@
+-- Retrieval Endpoints for navigating the Dictionary
---
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/Dictionaries/SpellListDict.lua
@@ -0,0 +1,21 @@
+SpellsDict = {}
+
+Cleric = {}
+Cleric.Cantrips.Base = "2f43a103-5bf1-4534-b14f-663decc0c525"
+Cleric.ChannelDivinity.Base = "239cc7cc-f642-4ddc-b091-498d81612c51"
+Cleric.ChannelDivinity.Life = "36795b9e-f712-44a4-9c0f-eda1e78df9ab"
+Cleric.ChannelDivinity.Light = "a322dbfe-69d0-421a-819a-4317b6c87a3b"
+Cleric.ChannelDivinity.Trickery = "c35cb4e6-2690-4e1f-bd89-69f4f69ae818"
+Cleric.Spells.Base.One = "269d1a3b-eed8-4131-8901-a562238f5289"
+Cleric.Spells.Base.Two = "2968a3e6-6c8a-4c2e-882a-ad295a2ad8ac"
+Cleric.Spells.Base.Three = "21be0992-499f-4c7a-a77a-4430085e947a"
+Cleric.Spells.Life.One = "26ffbe70-23c6-4918-9e6a-a3e556a9d355"
+Cleric.Spells.Life.Two = "bbaf0bab-b0b3-44c9-a567-304e35c063c9"
+Cleric.Spells.Life.Five = "a9ff6ed3-050e-4963-b37b-c083285d6214"
+Cleric.Spells.Light.One = "e2464518-2484-436e-9153-864c9dd05a7d"
+Cleric.Spells.Light.Two = "31037493-7a57-4dce-9412-4689c010199c"
+Cleric.Spells.Light.Five = "1a50d25a-27b5-4789-b2d6-532fac4437c6"
+Cleric.Spells.Trickery.One = "98ef625f-d127-4fc3-8e37-37a4b27e6393"
+Cleric.Spells.Trickery.Two = "9f603ac8-2e51-4ce9-b924-c8093148bdf5"
+Cleric.Spells.Trickery.Five = "8a9faf7a-aacf-43f4-a0dd-5ea7e72449f3"
+Wizard.ArcaneRecovery.Base = "34c3321d-75ab-4b50-a44d-cbac8705a360"
