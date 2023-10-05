c4b8bf6228d2e939385355513e7a4ed831b0f2ab
NellsRelo <nellsrelo@gmail.com>
2023-10-04 20:47:35 -0700

    Initialize Dictionaries and rename files

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/_init.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/_init.lua
@@ -11,5 +11,11 @@ Globals.ShowWarnings = false
 
 Ext.Require("Globals/Dictionaries.lua")
 Ext.Require("Globals/MulticlassDictionary.lua")
-Ext.Require("Globals/VanillaDictionary.lua")
 Ext.Require("Globals/Strings.lua")
+Ext.Require("Globals/Dictionaries/AbilityList.lua")
+Ext.Require("Globals/Dictionaries/EquipmentList.lua")
+Ext.Require("Globals/Dictionaries/Feat.lua")
+Ext.Require("Globals/Dictionaries/PassiveList.lua")
+Ext.Require("Globals/Dictionaries/Progression.lua")
+Ext.Require("Globals/Dictionaries/SpellList.lua")
+Ext.Require("Globals/Dictionaries/VanillaDictionary.lua")
