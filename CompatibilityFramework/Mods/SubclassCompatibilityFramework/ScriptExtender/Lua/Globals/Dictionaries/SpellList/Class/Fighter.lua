14fcd08e961830f88713635a54a7ceb165976450
NellsRelo <nellsrelo@gmail.com>
2023-10-16 10:43:29 -0700

    SharedDev SpellLists

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/Dictionaries/SpellList/Class/Fighter.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/Dictionaries/SpellList/Class/Fighter.lua
@@ -7,6 +7,8 @@ fighter.BattleMaster.Maneuvers = "b45be5b3-1f83-44f0-8520-6726f95c8ccb"
 
 fighter.EldritchKnight.Scrolls = "0e33c4d4-d784-4cfb-852e-e510dd100ef2"
 fighter.EldritchKnight.Cantrips = "6529c75a-d8cd-4ddb-a1b1-f55cb1e66d9f"
+fighter.EldritchKnight.WeaponBond = "42c2f7ed-8d06-4347-a912-01172a0e318b"
 DictUtils.InsertField(fighter.EldritchKnight.Spells, {
-  One = "32aeba85-13bd-4a6f-8e06-cd4447b746d8"
+  One = "32aeba85-13bd-4a6f-8e06-cd4447b746d8",
+  Two = "4a86443c-6a21-4b8d-b1bf-55a99e021354"
 })
--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/Dictionaries/SpellList/Feat/MagicInitiate.lua
+++
@@ -1,3 +0,0 @@
-local magicInitiate = DictUtils.SpellList("MagicInitiate", { "Bard", "Cleric", "Druid", "Sorcerer", "Warlock", "Wizard" })
-
-magicInitiate.Warlock.Spells = "21e1e2b9-81f7-47d2-ab69-90ba67c0c74c"
