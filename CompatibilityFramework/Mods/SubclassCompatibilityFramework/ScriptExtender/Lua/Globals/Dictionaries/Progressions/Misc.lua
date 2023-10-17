a1831c05e51bbd3e7080dd42c08b26364734ecb7
NellsRelo <nellsrelo@gmail.com>
2023-10-16 18:20:47 -0700

    Add Remainder of UUIDs from Shared Progression

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/Dictionaries/Progressions/Misc.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/Globals/Dictionaries/Progressions/Misc.lua
@@ -1,1 +1,16 @@
 local mcSpellSlots = DictUtils.Progression("MultiClassSpellSlots")
+
+Utils.MergeTables(mcSpellSlots.Base.Levels, {
+  One = DictUtils.setupClassLevelEntry("2366e69f-1248-41c5-b98a-abb5b79c8c29"),
+  Two = DictUtils.setupClassLevelEntry("efe53247-a533-4c43-a8bd-b20627b47305"),
+  Three = DictUtils.setupClassLevelEntry("05d31264-9517-4397-b433-a8dcdae60fd8"),
+  Four = DictUtils.setupClassLevelEntry("b2f60364-e4ff-4937-8821-506b24a08487"),
+  Five = DictUtils.setupClassLevelEntry("864c6164-4004-4848-a5da-9617c29d19ad"),
+  Six = DictUtils.setupClassLevelEntry("0c1f06ba-15bb-496e-9c8f-602411f54c7d"),
+  Seven = DictUtils.setupClassLevelEntry("f7b65569-9625-4508-b9b9-af22a6560078"),
+  Eight = DictUtils.setupClassLevelEntry("cca84498-363a-41ff-b43c-79d25bf896df"),
+  Nine = DictUtils.setupClassLevelEntry("982c4f35-dd12-4d6b-a7de-779d96de9229"),
+  Ten = DictUtils.setupClassLevelEntry("345e21ee-fec7-445f-985f-b975deaadfd5"),
+  Eleven = DictUtils.setupClassLevelEntry("1186d962-dfb8-4c4d-ad96-1ff50c9be698"),
+  Twelve = DictUtils.setupClassLevelEntry("a0d93679-f4f7-4314-8f86-3a440321bd45")
+})
