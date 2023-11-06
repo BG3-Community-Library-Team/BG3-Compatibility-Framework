995393783ee15cc9013084385fd1df0b3e612796
NellsRelo <nellsrelo@gmail.com>
2023-11-06 12:07:21 -0800

    Add Multi-UUID support for Feats

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/JsonLayer/FeatHandler.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/JsonLayer/FeatHandler.lua
@@ -1,22 +1,33 @@
+function FeatSubSectionHandler(data, featUUID, modGuid)
+  Utils.Info("Entering FeatJsonHandler")
+  if data.Selectors ~= nil then
+    for _, selector in pairs(data.Selectors) do
+      JsonUtils.ParseAndSubmitSelectors(selector, featUUID, modGuid, "Feat")
+    end
+  end
+
+  if data.Strings ~= nil then
+    for _, strings in pairs(data.Strings) do
+      JsonUtils.ParseAndSubmitStrings(strings, featUUID, modGuid, "Feat")
+    end
+  end
+
+  if data.Booleans ~= nil then
+    for _, boolean in pairs(data.Booleans) do
+      JsonUtils.ParseAndSubmitBoolean(boolean, featUUID, modGuid, "Feat")
+    end
+  end
+end
+
 function FeatJsonHandler(data, modGuid)
   Utils.Info("Entering FeatJsonHandler")
-  for _, feat in pairs(data) do
-    if feat.Selectors ~= nil then
-      for _, selector in pairs(feat.Selectors) do
-        JsonUtils.ParseAndSubmitSelectors(selector, feat.UUID, modGuid)
+  for _, feats in pairs(data) do
+    if feats.UUIDs ~= nil then
+      for _, uuid in pairs(feats.UUIDs) do
+        FeatSubSectionHandler(feats, uuid, modGuid)
       end
-    end
-
-    if feat.Strings ~= nil then
-      for _, strings in pairs(feat.Strings) do
-        JsonUtils.ParseAndSubmitStrings(strings, feat.UUID, modGuid, "Feat")
-      end
-    end
-
-    if data.Booleans ~= nil then
-      for _, boolean in pairs(data.Booleans) do
-        JsonUtils.ParseAndSubmitBoolean(boolean, data.UUIT, modGuid, "Feat")
-      end
+    elseif feats.UUID ~= nil then
+      FeatSubSectionHandler(feats, feats.UUID, modGuid)
     end
   end
 end
