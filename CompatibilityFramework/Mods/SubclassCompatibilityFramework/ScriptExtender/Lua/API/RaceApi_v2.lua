9b3fde9e7f6101a12e045d2adfcd549a94d8b015
NellsRelo <nellsrelo@gmail.com>
2024-01-24 22:48:28 -0800

    Actually fix Race API typo

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/API/RaceApi_v2.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/API/RaceApi_v2.lua
@@ -22,7 +22,7 @@ function Api.RemoveRaceChildData(payloads)
   CLUtils.Info("Entering API.RemoveRaceChildData")
 
   for _, payload in pairs(payloads) do
-    local err = CLUtils.DoValidation(payloads,
+    local err = CLUtils.DoValidation(payload,
       { Validators = { IsPayloadEmpty = CLStrings.ERROR_EMPTY_PAYLOAD, IsModLoaded = CLStrings.ERROR_MOD_NOT_LOADED } })
 
     if err ~= nil then
