c33dbb50ebcd18701adc8cd57c54e8c8cc90d786
NellsRelo <nellsrelo@gmail.com>
2024-01-24 19:35:38 -0800

    Ensure Race Child Removal handles validation correctly

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/CompatibilitySupport/SelectorSupport/_SelectorRemoval.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/CompatibilitySupport/SelectorSupport/_SelectorRemoval.lua
@@ -5,7 +5,7 @@ local function RemoveSelector(payload)
   if target ~= nil and CLUtils.CacheOrRetrieve(target, type) ~= nil then
     Utils.BuildQueueEntry(Globals.ModuleTypes[type], target, "Selectors_Remove", payload.Function)
     table.insert(Queue[Globals.ModuleTypes[type]][target].Selectors_Remove[payload.Function], payload.ListUUID)
-    if Queue[Globals.ModuleTypes[type]][target] ~= nil and Queue[Globals.ModuleTypes[type]][target].Selectors ~= nil then
+    if Queue[Globals.ModuleTypes[type]][target] ~= nil and Queue[Globals.ModuleTypes[type]][target].Selectors ~= nil and Queue[Globals.ModuleTypes[type]][target].Selectors[payload.Function] then
       for key, val in pairs(Queue[Globals.ModuleTypes[type]][target].Selectors[payload.Function]) do
         local uuid = val.UUID or val.SpellUUID
         if uuid == payload.ListUUID then
