6f8bdc0513aa163470b02d462bab20455c90e613
NellsRelo <nellsrelo@gmail.com>
2026-02-19 22:53:37 -0800

    Simplify Setting Fields

--- CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/CompatibilitySupport/BooleanSupport/FieldSet.lua
+++ CompatibilityFramework/Mods/SubclassCompatibilityFramework/ScriptExtender/Lua/CompatibilitySupport/BooleanSupport/FieldSet.lua
@@ -1,17 +1,12 @@
 
 local function SetField(payload)
 
-  if CLUtils.IsInTable(CLGlobals.FieldTypes, payload.Key) then
-    CLUtils.Info(Strings.PREFIX .. "Key is in table: " .. CLUtils.RetrieveModHandleAndAuthor(payload.modGuid))
-    local fleshedObject = CLUtils.CacheOrRetrieve(payload.Target, payload.FileType)
-    local queueType = Globals.ModuleTypes[payload.FileType]
+  local fleshedObject = CLUtils.CacheOrRetrieve(payload.Target, payload.FileType)
+  local queueType = Globals.ModuleTypes[payload.FileType]
 
-    if fleshedObject ~= nil then
-      Utils.BuildQueueEntry(queueType, payload.Target, "Fields")
-      Queue[queueType][payload.Target].Fields[payload.Key] = payload.Value
-    end
-  else
-    CLUtils.Warn(Strings.PREFIX .. "Field type not validated: " .. payload.Key)
+  if fleshedObject ~= nil then
+    Utils.BuildQueueEntry(queueType, payload.Target, "Fields")
+    Queue[queueType][payload.Target].Fields[payload.Key] = payload.Value
   end
 end
 
